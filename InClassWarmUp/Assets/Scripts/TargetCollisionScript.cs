b153b9730859a47118367fc64ada7a2b22a4187c
Zach <improvisedphilosophy@gmail.com>
2016-09-14 13:12:09 -0400

    Updating the collision script to start working

--- InClassWarmUp/Assets/Scripts/TargetCollisionScript.cs
+++ InClassWarmUp/Assets/Scripts/TargetCollisionScript.cs
@@ -3,16 +3,39 @@ using System.Collections;
 
 public class TargetCollisionScript : MonoBehaviour {
 
-	BoxCollider2D boxCol;
+//	BoxCollider2D boxCol;
+
+	//create a reference to the player and sfx
+	public GameObject player;
+	public AudioSource collidingMusic;
+	public AudioSource successMusic;
+
+	//create a timer to check how long this is colliding with the player
+	float collisionTimer;
+	//and a float so we can change the desired collision time in the inspector
+	public float collisionTimeLimit;
 
 	// Use this for initialization
 	void Start () {
 	
+		player = GameObject.Find ("Player");
+		collidingMusic = GetComponent<AudioSource> ();
+		successMusic = player.GetComponent<Audiosource> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	void OnCollisionEnter(Collision collider){
+		//if you're colliding, but not for the desired time limit...
+		if (collider.gameObject.name == "Player" && (collisionTimer < collisionTimeLimit)) {
+			//play and loop the colliding sfx
+			collidingMusic.Play ();
+			collidingMusic.loop (true);
+		}
 	}
 
 	void CheckIfCollidingWithPlayer(){
@@ -27,3 +50,16 @@ public class TargetCollisionScript : MonoBehaviour {
 		//TODO: Move this to a random spot in the box that's not colliding with the player
 	}
 }
+
+
+
+
+void OnCollisionEnter(Collision collider){
+	if (collider.gameObject.name == "Player" && touchedTheEnd == false) {
+		levelMusic.Stop ();
+		winMusic.Play();
+		touchedTheEnd = true;
+		Debug.Log ("You touched the end?");
+		gameManagerScript.CheckHighScore ();
+		Invoke ("Restart", 4f);
+	}
